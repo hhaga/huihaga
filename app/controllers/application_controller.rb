7f000a7bdfc3e930abb312bc4d696b62c6e8b483
hhaga <hakonhaga@gmail.com>
2011-11-06 23:10:57 +0100

    la til authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
   protect_from_forgery
+  
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "secret123"
+    end
+  end
 end
